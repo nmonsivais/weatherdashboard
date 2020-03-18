43b83abfb6a432fe131add286ceddb952862ddba
Tro <noe.monsivais@gmail.com>
2020-03-18 16:11:25 -0500

    made some updates.

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,30 +1,31 @@
 var APIKey = "cda32d604d850c123a48e324b1c48dbb";
 var queryURL =
-  "https://api.openweathermap.org/data/2.5/weather?q=Nashville,Tennessee&appid=" +
+  "https://api.openweathermap.org/data/2.5/weather?q=Nashville,Tennessee&units=imperial&appid=" +
   APIKey;
 var object =
-  "https://api.openweathermap.org/data/2.5/weather?q=Nashville,Tennessee&appid=cda32d604d850c123a48e324b1c48dbb";
+  "https://api.openweathermap.org/data/2.5/weather?q=Nashville,Tennessee&units=imperial&appid=cda32d604d850c123a48e324b1c48dbb";
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  var kelvinTemp = response.main.temp;
-  var fahrenheit = (kelvinTemp - 273.15) * 1.8;
-  var city = response.name;
-  var wind = response.wind.speed;
-  var humidity = response.main.humidity;
+function weather() {
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    var fahrenheit = response.main.temp;
+    var city = response.name;
+    var wind = response.wind.speed;
+    var humidity = response.main.humidity;
 
-  console.log(queryURL);
-  console.log(response);
-  console.log(response.name);
-  console.log(kelvinTemp);
-  console.log("The temperature in Fahrenheit is " + fahrenheit);
-  console.log(response.main.humidity);
-  console.log(response.wind.speed);
-
-  $(".city").text("City: " + city);
-  $(".wind").text("Wind: " + wind);
-  $(".humidity").text("Humidity: " + humidity);
-  $(".temp").text("Temperature: " + fahrenheit);
-});
+    document.getElementById("cont1");
+    $(".city").text("City: " + city);
+    $(".wind").text("Wind: " + wind);
+    $(".humidity").text("Humidity: " + humidity);
+    $(".temp").text("Temperature: " + fahrenheit);
+  });
+}
+//   console.log(queryURL);
+//   console.log(response);
+//   console.log(response.name);
+//   console.log(kelvinTemp);
+//   console.log("The temperature in Fahrenheit is " + fahrenheit);
+//   console.log(response.main.humidity);
+//   console.log(response.wind.speed);
